6bb48fc6d9d263571f4a97cbbc99076c85f4d731
jack do <jackie.duc.do@gmail.com>
2026-07-31 17:29:18 +1000

    feat: move to clean architecture

--- PaymentApp/Models/Requests.cs
+++
@@ -1,7 +0,0 @@
-namespace PaymentApp.Models;
-
-public record RegisterRequest(string Name, string Email, string Password);
-public record LoginRequest(string Email, string Password);
-public record TransferRequest(int PayerId, int PayeeId, decimal Amount);
-
-public record UserResponse(int Id, string Name, string Email);
--- PaymentApp/Services/Payment/IPaymentService.cs
+++
@@ -1,6 +0,0 @@
-namespace PaymentApp.Services.Payment;
-
-public interface IPaymentService
-{
-    Task TransferAsync(int payerId, int payeeUserId, decimal amount);
-}
